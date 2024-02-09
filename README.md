7af4b4afa8c7cf21d0f6869c6dfd127078bc8163
Joel Wang <jo@nn27.local>
2024-02-09 16:09:29 +0800

    working script

--- README.md
+++ README.md
@@ -4,22 +4,17 @@ Converts Kindle Vocabularies into Anki Cards
 
 ## What exactly does it do?
 
-For each pair of word and example from `vocab.db`:
+For each pair of word and example from `vocab.db` (from Kindle):
 
-1. Find the most likely meaning from a translation service.
-2. Determine the type of the word from the example (`habla` is verb or noun)
-3. Find the root form of the word (`habla` to `hablar` if it's a verb)
-4. Find it's entry from a ES-ES _and_ a ES-EN dictionary
-5. Combine all into well-formatted card
-6. Assemble cards into Anki format
+1. Find the meaning from RAE with [raebot](https://github.com/weiwei/raebot).
+2. Combine all into well-formatted card
+3. Assemble cards into Anki format
 
-## What dictionaries and translation services
+## Notes
 
 1. [MS Translator][https://docs.microsoft.com/en-us/azure/cognitive-services/translator/]
 2. [OL][https://languages.oup.com/google-dictionary-es/]
 3. [DLE][https://dle.rae.es/]
 4. ???
-
-## Notes
 
 The plan was to use spacy for step 2 and 3, but apparently spacy isn't accurate enough. Use Kindle word stem instead.
--- kindle2anki.py
+++ kindle2anki.py
@@ -3,12 +3,9 @@ from logging import debug
 import sys
 import sqlite3
 import argparse
-from pyrae import dle
 import time
-# import spacy
-
-# nlp = spacy.load("es_core_news_sm")
-
+from raebot import search_words
+import genanki
 from peewee import *
 import datetime
 
@@ -73,16 +70,64 @@ lookup = (Lookup
     .select(Lookup.usage, Word.stem, Word.word)
     .join(Word, JOIN.LEFT_OUTER, on=(Word.id == Lookup.word_key))
     .where(Word.lang == "es")
-    .where(Lookup.book_key == "Preterito_imperfecto:DE2DB34D")
+    # .where(Lookup.book_key == "Preterito_imperfecto:DE2DB34D")
     .group_by(Word.stem)
 )
 
-# https://developer.oxforddictionaries.com/documentation
-od_url = "https://od-api.oxforddictionaries.com/api/v2/entries/es/perro?strictMatch='false'"
-od_header = {"app_key": "04b62b3384b577e13c8c08721d7a38cf", "app_id": "52f6522b"}
+
+my_model = genanki.Model(
+  1607392321,
+  'Simple Model',
+  fields=[
+    {'name': 'Word'},
+    {'name': 'Example'},
+    {'name': 'Definition'},
+  ],
+  templates=[
+    {
+      'name': 'Card 1',
+      'qfmt': '<center>{{Word}}<center><br>{{Example}}',
+      'afmt': '{{FrontSide}}<hr id="answer">{{Definition}}',
+    },
+  ])
+
+my_deck = genanki.Deck(
+  2059400220,
+  'Kindle Vocabularies')
+
+unknown = []
+exceptions = []
 
 for l in lookup:
     print(l.word.stem)
-    res = dle.search_by_word(word=l.word.stem)
-    print(res.to_dict())
+    try:
+        res = search_words(l.word.stem)
+    except:
+        exceptions.append(l.word.stem)
+        continue
+    if not res:
+        unknown.append(l.word.stem)
+        continue
+    defis = res[0].definitions[:3]
+    dd =  ""
+    for defi in defis:
+        dd += "<p>("
+        dd += " ".join(defi.types)
+        dd += ") "
+        dd += defi.definition
+        if defi.examples:
+            dd += "<i>"
+            dd += " "
+            dd += " ".join(defi.examples)
+            dd += "</i>"
+        dd += "</p>"
+    my_note = genanki.Note(
+        model=my_model,
+        fields=[l.word.stem, l.usage, dd]
+    )
+    my_deck.add_note(my_note)
+    time.sleep(2)
 
+genanki.Package(my_deck).write_to_file('output.apkg')
+open("unknown.txt", "w").write("\n".join(unknown))
+open("exceptions.txt", "w").write("\n".join(exceptions))
